c96167f20a90198304b24aa65eae560ae9287af2
CJDAutorio <cj.dautorio2021@gmail.com>
2023-08-17 07:05:30 -0400

    Create PageLayout.jsx

---
+++ src/components/PageLayout.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from 'react'
+import ProjectForm from './ProjectForm'
+
+export default function Example() {
+  return (
+    <>
+    <div className="min-h-screen flex items-center justify-center">
+        <ProjectForm />
+    </div>
+    </>
+  )
+}

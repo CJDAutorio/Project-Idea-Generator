63583da5a98956cbea3ac51a89fc87a47f43408f
CJDAutorio <cj.dautorio2021@gmail.com>
2023-08-21 07:31:57 -0400

    Increased margin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+        <footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-5">
             <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div class="sm:flex sm:items-center sm:justify-between">
                     <a href="https://www.projectideagenerator.com/" class="flex items-center mb-4 sm:mb-0">
@@ -8,7 +8,7 @@ export default function Footer() {
                     </a>
                     <div class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                         <p>Created by&nbsp;<a href="https://www.cjdautor.io" class="mr-5 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">CJ DAutorio</a></p>
-                        
+                        <p></p>
                     </div>
                 </div>
             </div>

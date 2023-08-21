8771edb3b61057b4755aefbee201703322b5e6fc
CJDAutorio <cj.dautorio2021@gmail.com>
2023-08-21 07:25:24 -0400

    Update PageLayout.jsx

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,11 +1,13 @@
 import ProjectForm from './ProjectForm'
+import Footer from './Footer'
 
 export default function PageLayout() {
     return (
         <>
-            <div className="flex flex-col items-center justify-center mt-10">
+            <div className="flex flex-col items-center justify-center grow">
                 <ProjectForm />
             </div>
+            <Footer />
         </>
     )
 }

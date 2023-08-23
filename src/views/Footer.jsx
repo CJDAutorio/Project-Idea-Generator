64dda253050965f1b348a0018a61952f97a2041a
CJDAutorio <cj.dautorio2021@gmail.com>
2023-08-23 18:16:41 -0400

    Footer now only contains author, adjusted appearance

--- src/views/Footer.jsx
+++ src/views/Footer.jsx
@@ -1,13 +1,10 @@
 export default function Footer() {
     return (
-        <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-5">
-            <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <footer className="bg-white shadow dark:bg-gray-900 mt-6">
+            <div className="w-full max-w-screen-xl mx-auto px-4 pt-2 md:pt-2">
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <a href="https://www.projectideagenerator.com/" className="flex items-center mb-4 sm:mb-0">
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Project Idea Generator</span>
-                    </a>
-                    <div className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                        <p>Created by&nbsp;<a href="https://www.cjdautor.io" className="mr-5 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">CJ DAutorio</a></p>
+                    <div className="flex flex-wrap items-center pb-3 text-sm font-medium sm:mb-0 text-gray-400">
+                        <p>Created by&nbsp;<a href="https://www.cjdautor.io" className="hover:text-white text-gray-400">CJ DAutorio</a></p>
                     </div>
                 </div>
             </div>

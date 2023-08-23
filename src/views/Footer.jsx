e7e45587d9a7249d943ecfc58658555ba70beaa9
CJDAutorio <cj.dautorio2021@gmail.com>
2023-08-23 17:44:05 -0400

    Updated class props

--- src/views/Footer.jsx
+++ src/views/Footer.jsx
@@ -1,14 +1,13 @@
 export default function Footer() {
     return (
-        <footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-5">
-            <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-                <div class="sm:flex sm:items-center sm:justify-between">
-                    <a href="https://www.projectideagenerator.com/" class="flex items-center mb-4 sm:mb-0">
-                        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Project Idea Generator</span>
+        <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-5">
+            <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+                <div className="sm:flex sm:items-center sm:justify-between">
+                    <a href="https://www.projectideagenerator.com/" className="flex items-center mb-4 sm:mb-0">
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Project Idea Generator</span>
                     </a>
-                    <div class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                        <p>Created by&nbsp;<a href="https://www.cjdautor.io" class="mr-5 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">CJ DAutorio</a></p>
-                        <p></p>
+                    <div className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                        <p>Created by&nbsp;<a href="https://www.cjdautor.io" className="mr-5 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">CJ DAutorio</a></p>
                     </div>
                 </div>
             </div>

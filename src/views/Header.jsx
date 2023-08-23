8a28eeda00dcec45b11c45b50b2cfc82f59f14d2
CJDAutorio <cj.dautorio2021@gmail.com>
2023-08-23 18:16:27 -0400

    Adjusted header appearance, removed author

--- src/views/Header.jsx
+++ src/views/Header.jsx
@@ -2,17 +2,14 @@ import Logo from '../assets/img/icon_nobg.png';
 
 export default function Header() {
     return (
-        <header className="bg-white shadow dark:bg-gray-900 ">
+        <header className="shadow bg-gray-900 ">
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-                <div className="sm:flex sm:items-center sm:justify-between">
-                    <a href="https://www.projectideagenerator.com/" className="flex items-center mb-4 sm:mb-0">
+                <div className="flex flex-col md:flex-row md:items-center md:justify-between">
+                    <a href="https://www.projectideagenerator.com/" className="flex items-center mb-4 md:mb-0">
                         <img src={Logo} alt="Project Idea Generator Logo" className='h-10 pr-6' />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Project Idea Generator</span>
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap text-white pr-6">Project Idea Generator</span>
                     </a>
-                    <div className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                        <p>Created by&nbsp;<a href="https://www.cjdautor.io" className="mr-5 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">CJ DAutorio</a></p>
-                        <p></p>
-                    </div>
+                        <p className='text-gray-400'>GPT-3 generated practice projects.</p>
                 </div>
             </div>
         </header>

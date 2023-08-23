b8102f674540001de66004dafa0f5301ff454acb
CJDAutorio <cj.dautorio2021@gmail.com>
2023-08-23 17:43:54 -0400

    Added header, disabled footer

--- src/views/PageLayout.jsx
+++ src/views/PageLayout.jsx
@@ -1,13 +1,15 @@
 import ProjectForm from './ProjectForm'
+import Header from './Header'
 import Footer from './Footer'
 
 export default function PageLayout() {
     return (
         <>
+            <Header />
             <div className="flex flex-col items-center justify-center grow">
                 <ProjectForm />
             </div>
-            <Footer />
+            {/* <Footer /> */}
         </>
     )
 }

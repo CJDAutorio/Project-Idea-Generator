c0b9e2703c4ada8a87aee49f4388f28813f0a13f
CJDAutorio <cj.dautorio2021@gmail.com>
2023-08-23 17:32:00 -0400

    Moved views from components to views folder

--- src/views/Footer.jsx
+++ src/views/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
             <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div class="sm:flex sm:items-center sm:justify-between">
                     <a href="https://www.projectideagenerator.com/" class="flex items-center mb-4 sm:mb-0">
-                        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">ProjectIdeaGenerator</span>
+                        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Project Idea Generator</span>
                     </a>
                     <div class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                         <p>Created by&nbsp;<a href="https://www.cjdautor.io" class="mr-5 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">CJ DAutorio</a></p>
